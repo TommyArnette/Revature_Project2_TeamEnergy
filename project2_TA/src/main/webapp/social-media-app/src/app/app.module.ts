41ea398d4da645111e36e80799deff3945968493
archermichael <archer.v.michael@gmail.com>
2021-09-08 21:27:03 -0500

    login and register form created and styled

--- project2_TA/src/main/webapp/social-media-app/src/app/app.module.ts
+++ project2_TA/src/main/webapp/social-media-app/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RegisterModalComponent } from './components/register-modal/register-modal.component';
+import { PostFeedComponent } from './components/post-feed/post-feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    RegisterModalComponent
+    RegisterModalComponent,
+    PostFeedComponent
   ],
   imports: [
     BrowserModule,
