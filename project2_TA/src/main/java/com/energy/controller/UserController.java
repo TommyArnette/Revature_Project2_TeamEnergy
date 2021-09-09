40148c4cf82d9ca166c395c0b3684d4b7db96581
Milo Cao <qic43@pitt.edu>
2021-09-08 22:17:26 -0400

    update h2

--- project2_TA/src/main/java/com/energy/controller/UserController.java
+++ project2_TA/src/main/java/com/energy/controller/UserController.java
@@ -10,7 +10,7 @@ import javax.mail.MessagingException;
 import javax.servlet.http.HttpSession;
 
 @RestController("userController")
-@CrossOrigin(value = "http://localhost:9000", allowCredentials = "true")
+@CrossOrigin(value = "http://localhost:4200/", allowCredentials = "true")
 public class UserController {
     private UserService userService;
 
