4d06f05b2005fd2f2ef1c258c87aee0b79277eab
Tommy Arnette <tommy.arnette@gmail.com>
2021-09-08 20:06:39 -0400

    Added PostService with getAllPosts(), getPostsByUserId(), and addNewPost(). Created PostController with same methods

--- project2_TA/.idea/sonarlint/issuestore/index.pb
+++ project2_TA/.idea/sonarlint/issuestore/index.pb
@@ -5,5 +5,3 @@ Z
 *src/main/java/com/energy/models/Posts.java,1\5\1592dd0dada0c5b535f5c1ebaf48f83aaf19887d
 _
 /src/main/java/com/energy/service/S3Service.java,5\5\55f7020a2035a3b09d0c933ae5dae4215abab310
-Y
-)src/main/java/com/energy/dao/UserDao.java,f\9\f93b05f101a7ee876d3bda736848d8bcc587b9e9
--- project2_TA/src/main/java/com/energy/controller/PostController.java
+++ project2_TA/src/main/java/com/energy/controller/PostController.java
@@ -1,5 +1,53 @@
 package com.energy.controller;
 
-public class PostController {
+import com.energy.models.JsonResponse;
+import com.energy.models.Post;
+import com.energy.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController("postController")
+@CrossOrigin(value = "http://localhost:9000", allowCredentials = "true")
+public class PostController {
+    private PostService postService;
+
+    @Autowired
+    public PostController(PostService postService){
+        this.postService = postService;
+    }
+
+    @GetMapping("post")
+    public JsonResponse getAllPosts(){
+        return new JsonResponse(true, "All posts have been retrieved.", this.postService.getAllPosts());
+    }
+
+    @GetMapping("post/{userId}")
+    public JsonResponse getPostsByUserId(@PathVariable Integer userId){
+        JsonResponse jsonResponse;
+
+        List<Post> userPostList = this.postService.getPostsByUserId(userId);
+
+        if(userPostList != null){
+            jsonResponse = new JsonResponse(true, "Posts for user with user ID: " + userId + " obtained.", userPostList);
+        }else{
+            jsonResponse = new JsonResponse(false, "No posts obtained.", null);
+        }
+        return jsonResponse;
+    }
+
+    @PostMapping("post")
+    public JsonResponse createPost(@RequestBody Post post){
+        JsonResponse jsonResponse;
+        this.postService.addNewPost(post);
+
+        if(post != null){
+            jsonResponse = new JsonResponse(true, "Post has been created.", post);
+        }
+        else {
+            jsonResponse = new JsonResponse(false, "Post was empty or could not be created.", null);
+        }
+        return jsonResponse;
+    }
 }
