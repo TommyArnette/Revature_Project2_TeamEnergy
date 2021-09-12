49b3d91032570c788621fa7684f925f8af41c73a
archermichael <archer.v.michael@gmail.com>
2021-09-12 13:16:06 -0500

    removing pre spring backend

--- project2_TA/src/main/java/com/energy/util/S3Util.java
+++
@@ -1,8 +0,0 @@
-package com.energy.util;
-
-public class S3Util {
-    public static String awsID = "AKIA2PU5D6KIILOLLEOU"; // see discord for all of these values. They shouldn't be pushed to github
-    public static String awsKey = "DO1u9DIDq4M01GI1ut0zMSbII7TYYylS9Bl3wxMO";
-    public static String region = "us-east-2";
-    public static String bucketName = "teamenergybucket";
-}
