35ebe4613bbdef82681745a40b2646409afededa
Tommy Arnette <tommy.arnette@gmail.com>
2021-09-12 15:07:32 -0400

    Minor changes to JsonResponse messages for clarity.

--- refact/src/main/java/com/revature/controller/UserController.java
+++ refact/src/main/java/com/revature/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
 
     @GetMapping("user")
     public JsonResponse selectAllUsers(){
-        return new JsonResponse(true,"Obtained all users",this.userService.selectAllUsers());
+        return new JsonResponse(true,"Obtained all users.",this.userService.selectAllUsers());
     }
 
     @PostMapping("user")
@@ -26,10 +26,10 @@ public class UserController {
         JsonResponse jsonResponse;
         User tempU = this.userService.registerNewUser(user);
         if(tempU==null){
-            jsonResponse =new JsonResponse(false,"username already exist", null);
+            jsonResponse =new JsonResponse(false,"Cannot create, username already exists.", null);
         }else{
             session.setAttribute("loggedInUser",tempU);
-            jsonResponse= new JsonResponse(true,"user created",user);
+            jsonResponse= new JsonResponse(true,"User registered, session created.",user);
         }
         return jsonResponse;
     }
@@ -48,9 +48,9 @@ public class UserController {
         JsonResponse jsonResponse;
         User user = this.userService.selectUserById(userId);
         if(user != null){
-            jsonResponse = new JsonResponse(true,"user found",user);
+            jsonResponse = new JsonResponse(true,"Found user.",user);
         }else{
-            jsonResponse = new JsonResponse(false,"user not found",null);
+            jsonResponse = new JsonResponse(false,"No user found.",null);
         }
         return jsonResponse;
     }
@@ -59,9 +59,9 @@ public class UserController {
         JsonResponse jsonResponse;
         User user = this.userService.selectUserByUsername(username);
         if(user != null){
-            jsonResponse = new JsonResponse(true,"user found",user);
+            jsonResponse = new JsonResponse(true,"Found user.",user);
         }else{
-            jsonResponse = new JsonResponse(false,"user not found",null);
+            jsonResponse = new JsonResponse(false,"No user found.",null);
         }
         return jsonResponse;
     }
@@ -73,9 +73,9 @@ public class UserController {
         User tUser = this.userService.login(user);
 
         if (tUser == null) {
-            jsonResponse = new JsonResponse(false,"data incorrect",null);
+            jsonResponse = new JsonResponse(false,"Incorrect credentials.",null);
         }else{
-            jsonResponse= new JsonResponse(true, "login and session created",tUser);
+            jsonResponse= new JsonResponse(true, "Login and session created.",tUser);
             session.setAttribute("loggedInUser",tUser);
         }
 
@@ -85,7 +85,7 @@ public class UserController {
     @GetMapping("logout")
     public JsonResponse logout(HttpSession session){
         session.setAttribute("loggedInUser",null);
-        return new JsonResponse(true,"session terminated",null);
+        return new JsonResponse(true,"Session terminated.",null);
     }
 
     @GetMapping("check-session")
@@ -93,9 +93,9 @@ public class UserController {
         JsonResponse jsonResponse;
         User user = (User) session.getAttribute("loggedInUser");
         if(user !=null){
-            jsonResponse = new JsonResponse(true,"session found",user);
+            jsonResponse = new JsonResponse(true,"Session found.",user);
         }else{
-            jsonResponse = new JsonResponse(false, "no session found",null);
+            jsonResponse = new JsonResponse(false, "No session found.",null);
         }
         return jsonResponse;
     }
