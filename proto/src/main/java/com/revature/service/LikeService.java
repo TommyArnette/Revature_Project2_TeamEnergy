11115aec6e838ddc4908bbf48c2feb143a57bc1b
Tommy Arnette <tommy.arnette@gmail.com>
2021-09-15 00:05:58 -0400

    Service JavaDocs complete (open to criticism)

--- proto/src/main/java/com/revature/service/LikeService.java
+++ proto/src/main/java/com/revature/service/LikeService.java
@@ -10,12 +10,27 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Using the Spring Stereotype @Service
+ *
+ * This class handles our Like Service layer and references the LikeDao to obtain information.
+ */
 @Service("likeService")
 public class LikeService {
     private LikeDao likeDao;
 
+    /**
+     * Autowired this constructor to advise Spring to handle Dependency Injection
+     * @param likeDao   Passes the information obtained from the LikeDao to this constructor
+     */
     @Autowired
     public LikeService(LikeDao likeDao){this.likeDao = likeDao;}
 
+    /**
+     * This method creates a Like object
+     *
+     * @param likes Refers to a new Like object
+     * @return      Returns a Like objects
+     */
     public Likes createLike(Likes likes){return this.likeDao.save(likes);}
 }
