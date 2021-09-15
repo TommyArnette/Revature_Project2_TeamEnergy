3300a71547a01f399bd87f9e4321fae8b76f29fe
Tommy Arnette <tommy.arnette@gmail.com>
2021-09-15 10:51:00 -0400

    Added required dependency for MockitoExtension

--- proto/src/main/java/com/revature/service/UserServiceTest.java
+++ proto/src/main/java/com/revature/service/UserServiceTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
