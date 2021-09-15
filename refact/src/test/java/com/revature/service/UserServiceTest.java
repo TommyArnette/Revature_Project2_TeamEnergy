3baed3673dcbf40fd96c263373e6c69eab22cd8a
Preston Bonomo <pcbonomo@gmail.com>
2021-09-14 20:21:23 -0400

    pushing test

---
+++ refact/src/test/java/com/revature/service/UserServiceTest.java
@@ -0,0 +1,77 @@
+package com.revature.service;
+
+
+
+import com.revature.models.User;
+import com.revature.repository.UserDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    UserService userService;
+
+    @Mock
+    UserDao userDao;
+
+    @BeforeEach
+    void setUp(){
+        this.userService = new UserService(userDao);
+    }
+
+
+    @Test
+    void selectAllUsers() {
+        //assign
+        List<User> expectedResult = new ArrayList<>();
+        expectedResult.add(new User(1,"test","pass123","preston","bonomo","pcbonomo@gmail.com","profile about me","profile pic"));
+        Mockito.when(userDao.findAll()).thenReturn(expectedResult);
+
+        //act
+        List<User> actualResult = this.userService.selectAllUsers();
+
+        //assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    void selectUserByIdNotNull() {
+        //assign
+        Integer id = 1;
+        User expectedResult = new User(1,"test","pass123","preston","bonomo","pcbonomo@gmail.com","profile about me","profile pic");
+        Mockito.when(userDao.findById(id)).thenReturn(Optional.of(expectedResult));
+        //act
+        User actualResult = this.userService.selectUserById(id);
+
+        //assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    void selectUserByIdNull(){
+        //assign
+        Integer id = 1;
+        User expectedResult = null;
+        Mockito.when(userDao.findById(id)).thenReturn(Optional.empty());
+        //act
+        User actualResult = this.userService.selectUserById(id);
+
+        //assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+}
